9c862e4e432a3b34ba72e928e4d54825a6bc7b1e
gracepal <131278381+gracepal@users.noreply.github.com>
2024-02-07 16:34:12 -0800

    Intro Component with Signup Form (#39)
    * starter files
    
    * basic starter setup
    
    * setup structure
    
    * setup media breakpoints
    
    * update readme todos checklist
    
    * form input validation styling
    
    * handle links
    
    * fix email error priority, update readme
    
    * add mobile w/errors screenshot

---
+++ 19-intro-component-with-signup-form/README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- ✅ View the optimal layout for the site depending on their device's screen size
+- ✅ See hover states for all interactive elements on the page
+- ✅ Receive an error message when the `form` is submitted if:
+  - ✅ Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
+  - ✅ The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
+
+### Screenshot
+
+### Desktop with Errors
+
+<img src="https://github.com/gracepal/frontend-mentor/assets/131278381/1f9c02e3-48ef-4a44-8821-236846934d3b" width="600" alt="intro-component-with-signup-form-desktop-with-errors">
+
+### Desktop with More Errors and Button Press
+
+<img width="600" alt="intro-component-with-signup-form-desktop-with-more-errors-and-buttonpress" src="https://github.com/gracepal/frontend-mentor/assets/131278381/13da0d31-21d5-485d-96b8-b72b0f1289fb">
+
+### Mobile with Initial / No Errors
+
+<img src="https://github.com/gracepal/frontend-mentor/assets/131278381/09862629-9efa-4244-aac4-e929297cb914" width="300" alt="intro-component-with-signup-form-mobile">
+
+### Mobile with Errors
+
+<img src="https://github.com/gracepal/frontend-mentor/assets/131278381/66995090-247a-42c0-a35b-e364f577e088" alt="intro-component-with-signup-form-mobile-with-errors" width="300">
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
---
+++ 19-intro-component-with-signup-form/index.js
@@ -0,0 +1,77 @@
+const formEl = document.querySelector('form')
+const allInputEls = document.querySelectorAll('input')
+const allLabelEls = document.querySelectorAll('label')
+const firstNameInputEl = document.querySelector('input[name=firstname]')
+const lastNameInputEl = document.querySelector('input[name=lastname]')
+const emailInputEl = document.querySelector('input[name=email]')
+const passwordInputEl = document.querySelector('input[name=password]')
+
+function getDigits(value) {
+  // Return digits before any period from the input, or 0 if none or empty
+  const source = value.toString()
+  let digits = source.match(/\d+(?=\.)|\d+/)
+  return digits ? parseInt(digits.join('')) : 0
+}
+
+function isValidEmail(email) {
+  // Source: https://stackoverflow.com/a/46181
+  return String(email)
+    .toLowerCase()
+    .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+}
+
+function removeAllFieldStyling() {
+  console.log('+++ removing all styling +++')
+  allLabelEls.forEach((el) => {
+    el.classList.remove('notify-error')
+    el.classList.remove('notify-success')
+    el.classList.remove('field-empty')
+    el.classList.remove('field-email-invalid')
+    el.setAttribute('style', `margin-bottom: 0px`)
+  })
+}
+
+function applyFieldStyling(targetEl, addStyles, setMarginBottom) {
+  for (const currStyle of addStyles) {
+    targetEl.classList.add(currStyle)
+  }
+  targetEl.setAttribute('style', `margin-bottom: ${setMarginBottom}px`)
+}
+
+formEl.addEventListener('submit', function (e) {
+  // ! NOTE !: updating margin-bottom here means, need to ensure not setting it elsewhere
+  // since the basis of whether or not to leave along or reset is based on if its current value is 0
+  e.preventDefault()
+  //   console.log('event', e)
+  const formData = new FormData(formEl)
+  for (const [k, v] of formData.entries()) {
+    // console.log(k, `"${v}"`)
+    const inputVal = v.trim()
+    const labelEl = document.querySelector(`label[for=${k}]`)
+    const labelElLoc = labelEl.getBoundingClientRect()
+    const currLabelHeightHalf = getDigits(labelElLoc.height / 2) // because error message takes up 50% height currently
+    const currLabelMarginBottom = getDigits(window.getComputedStyle(labelEl).marginBottom)
+    const currFormGap = getDigits(window.getComputedStyle(formEl).gap)
+    const newMarginBottom = currLabelMarginBottom == 0 ? currLabelMarginBottom + currFormGap + currLabelHeightHalf : 0
+
+    if (k == 'email' && inputVal.length > 0 && !isValidEmail(inputVal)) {
+      applyFieldStyling(labelEl, ['notify-error', 'field-email-invalid'], newMarginBottom)
+    } else if (inputVal == '') {
+      applyFieldStyling(labelEl, ['notify-error', 'field-empty'], newMarginBottom)
+    } else {
+      applyFieldStyling(labelEl, ['notify-success'], 0)
+    }
+  }
+})
+
+// Upon first/re-attempting to input values, clear/reset all validation styling
+allInputEls.forEach((inputEl) => {
+  inputEl.addEventListener('focus', function (e) {
+    removeAllFieldStyling()
+  })
+})
+
+// Also reset styling on any click, to also handle link clicks
+document.addEventListener('click', function () {
+  removeAllFieldStyling()
+})
