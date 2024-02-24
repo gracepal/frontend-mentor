0f1370cd553738bfd0abc9790467c68cb7f776a8
gracepal <gracepal810@gmail.com>
2024-02-24 14:24:09 -0800

    adviceslip api

--- 24-advice-generator-app/README.md
+++ 24-advice-generator-app/README.md
@@ -17,7 +17,7 @@ Users should be able to:
 
 - [ ] View the optimal layout for the app depending on their device's screen size
 - [x] See hover states for all interactive elements on the page
-- [ ] Generate a new piece of advice by clicking the dice icon
+- [x] Generate a new piece of advice by clicking the dice icon
 
 ### Screenshot
 
--- 24-advice-generator-app/script.js
+++ 24-advice-generator-app/script.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const buttonEl = document.querySelector('button')
+const adviceIdEl = document.querySelector('.id')
+const quoteEl = document.querySelector('.quote')
+const apiUrl = 'https://api.adviceslip.com/advice'
+
+buttonEl.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.slip)
+      const { id: idVal, advice: adviceVal } = data.slip
+      adviceIdEl.textContent = idVal
+      quoteEl.textContent = adviceVal
+    })
+    .catch((error) => console.log(error))
+})
