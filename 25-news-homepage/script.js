c83313088f7bd0acf4d0db8aa257b0d5110003a9
gracepal <gracepal810@gmail.com>
2024-02-25 09:42:46 -0800

    hamburger animation toggle

---
+++ 25-news-homepage/script.js
@@ -0,0 +1,10 @@
+const hamburgerEl = document.querySelector('.hamburger-btn')
+const bar1 = document.querySelector('.hamburger-btn .bar:nth-of-type(1)')
+const bar2 = document.querySelector('.hamburger-btn .bar:nth-of-type(2)')
+const bar3 = document.querySelector('.hamburger-btn .bar:nth-of-type(3)')
+
+hamburgerEl.addEventListener('click', () => {
+  bar1.classList.toggle('animateBar1')
+  bar2.classList.toggle('animateBar2')
+  bar3.classList.toggle('animateBar3')
+})
