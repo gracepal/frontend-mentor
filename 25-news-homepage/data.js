4ccc15a8661dfd9267fe345db14784ddec912837
gracepal <gracepal810@gmail.com>
2024-02-24 21:25:22 -0800

    initial setup

---
+++ 25-news-homepage/data.js
@@ -0,0 +1,55 @@
+const data = {
+  data: {
+    main: {
+      title: 'The Bright Future of Web 3.0?',
+      description: 'We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?',
+      button: 'Read more',
+    },
+    images: {
+      logo: '',
+      main: '',
+      topics: ['', '', ''],
+    },
+    navlinks: {
+      Home: '',
+      New: '',
+      Popular: '',
+      Trending: '',
+      Categories: '',
+    },
+    new: {
+      title: 'New',
+      articles: [
+        {
+          title: 'Hydrogen VS Electric Cars',
+          line: 'Will hydrogen-fueled cars ever catch up to EVs?',
+        },
+        {
+          title: 'The Downsides of AI Artistry',
+          line: 'What are the possible adverse effects of on-demand AI image generation?',
+        },
+        {
+          title: 'Is VC Funding Drying Up?',
+          line: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.',
+        },
+      ],
+      topics: [
+        {
+          id: '01',
+          title: 'Reviving Retro PCs',
+          line: 'What happens when old PCs are given modern upgrades?',
+        },
+        {
+          id: '02',
+          title: 'Top 10 Laptops of 2022',
+          line: 'Our best picks for various needs and budgets.',
+        },
+        {
+          id: '03',
+          title: 'The Growth of Gaming',
+          line: 'How the pandemic has sparked fresh opportunities.',
+        },
+      ],
+    },
+  },
+}
