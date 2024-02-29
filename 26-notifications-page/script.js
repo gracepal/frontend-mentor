41f5833a4d31394b869235836fe0f04a5ff74167
gracepal <gracepal810@gmail.com>
2024-02-28 20:06:02 -0800

    setup ui and hover states

--- 26-notifications-page/script.js
+++ 26-notifications-page/script.js
@@ -1,1 +1,5 @@
-console.log('Hello world! - Notifications here')
+const markAllBtn = document.querySelector('.mark-all')
+
+markAllBtn.addEventListener('click', function () {
+  console.log('Clicked button to mark all')
+})
