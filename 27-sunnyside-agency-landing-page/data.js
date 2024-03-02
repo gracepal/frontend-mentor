9c699b9195788b09a162814f4997acfd0ad08ae5
gracepal <gracepal810@gmail.com>
2024-03-02 06:12:20 -0800

    v1 sunnyside landing

---
+++ 27-sunnyside-agency-landing-page/data.js
@@ -0,0 +1,109 @@
+const data = {
+  title: 'We are creatives',
+  image: './images/mobile/image-header.jpg',
+  navlinks: [
+    {
+      name: 'About',
+      source: '#',
+    },
+    {
+      name: 'Services',
+      source: '#',
+    },
+    {
+      name: 'Projects',
+      source: '#',
+    },
+    {
+      name: 'Contact',
+      source: '#',
+    },
+  ],
+  cards: [
+    {
+      title: 'Transform your brand',
+      message: 'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.',
+      button: 'Learn more',
+      image: './images/mobile/image-transform.jpg',
+    },
+    {
+      title: 'Stand out to the right audience',
+      message: 'Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.',
+      button: 'Learn more',
+      image: './images/mobile/image-stand-out.jpg',
+    },
+    {
+      title: 'Graphic design',
+      message: 'Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.',
+      image: './images/mobile/image-graphic-design.jpg',
+    },
+    {
+      title: 'Photography',
+      message: 'Increase your credibility by getting the most stunning, high-quality photos that improve your business image.',
+      image: './images/mobile/image-photography.jpg',
+    },
+  ],
+  testimonials: {
+    title: 'Client testimonials',
+    clients: [
+      {
+        name: 'Emily R.',
+        profile: 'Marketing Director',
+        avatar: './images/image-emily.jpg',
+        message: 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.',
+      },
+      {
+        name: 'Thomas S.',
+        profile: 'Chief Operating Officer',
+        avatar: './images/image-thomas.jpg',
+        message: 'Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.',
+      },
+      {
+        name: 'Jennie F.',
+        profile: 'Business Owner',
+        avatar: './images/image-jennie.jpg',
+        message: 'Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!',
+      },
+    ],
+  },
+  gallery: [
+    {
+      alt: 'milk bottles',
+      source: './images/mobile/image-gallery-milkbottles.jpg',
+    },
+    {
+      alt: 'orange on plate',
+      source: './images/mobile/image-gallery-orange.jpg',
+    },
+    {
+      alt: 'icecream cone',
+      source: './images/mobile/image-gallery-cone.jpg',
+    },
+    {
+      alt: 'sugar cubes',
+      source: './image-gallery-sugar-cubes.jpg',
+    },
+  ],
+  social: [
+    {
+      alt: 'facebook',
+      link: '#',
+      icon: './images/icon-facebook.svg',
+    },
+    {
+      alt: 'instagram',
+      link: '#',
+      icon: './images/icon-pinterest.svg',
+    },
+    {
+      alt: 'twitter',
+      link: '#',
+      icon: './images/icon-twitter.svg',
+    },
+    {
+      alt: 'pinterest',
+      link: '#',
+      icon: './images/icon-instagram.svg',
+    },
+  ],
+}
