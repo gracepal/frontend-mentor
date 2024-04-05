dc7aaaa39816c2af66d70243cf6f447a34720aa6
gracepal <gracepal810@gmail.com>
2024-04-05 03:58:39 -0700

    link buttons with data

--- 32-time-tracking-dashboard/script.js
+++ 32-time-tracking-dashboard/script.js
@@ -5,18 +5,44 @@ let dashboardData = null
 let dashboardTimeframe = 'weekly'
 
 btnDaily.addEventListener('click', function () {
-  console.log('Clicked button: Daily')
+  document.querySelectorAll('.links a').forEach((alink) => {
+    alink.classList.remove('active')
+  })
+  btnDaily.classList.add('active')
+  udpateDashboard('daily')
 })
 
 btnWeekly.addEventListener('click', function () {
-  console.log('Clicked button: Weekly')
+  document.querySelectorAll('.links a').forEach((alink) => {
+    alink.classList.remove('active')
+  })
+  btnWeekly.classList.add('active')
+  udpateDashboard('weekly')
 })
 
 btnMonthly.addEventListener('click', function () {
-  console.log('Clicked button: Monthly')
+  document.querySelectorAll('.links a').forEach((alink) => {
+    alink.classList.remove('active')
+  })
+  btnMonthly.classList.add('active')
+  udpateDashboard('monthly')
 })
 
-function udpateDashboard(timeframe) {}
+function udpateDashboard(timeframe) {
+  for (const { title, timeframes } of dashboardData) {
+    const contentKey = title.replace(/\s/g, '').toLowerCase()
+    const contentEl = document.querySelector(`.card.${contentKey}`)
+    const currentHrsEl = contentEl.children[0].children[2]
+    const previousTypeEl = contentEl.children[0].children[3].children[0]
+    const previousHrsEl = contentEl.children[0].children[3].children[1]
+
+    const currentHrsVal = timeframes[timeframe].current
+    const previousHrsVal = timeframes[timeframe].previous
+    currentHrsEl.textContent = `${currentHrsVal}${currentHrsVal == '1' ? 'hr' : 'hrs'}`
+    previousTypeEl.textContent = ['weekly', 'monthly'].includes(timeframe) ? timeframe.slice(0, -2) : 'day'
+    previousHrsEl.textContent = `${previousHrsVal}${previousHrsVal == '1' ? 'hr' : 'hrs'}`
+  }
+}
 
 async function loadData() {
   await fetch('./data.json')
@@ -24,7 +50,6 @@ async function loadData() {
     .then((data) => {
       dashboardData = data
       for (const { title, timeframes } of dashboardData) {
-        console.log(title, timeframes)
         const contentKey = title.replace(/\s/g, '').toLowerCase()
         const contentEl = document.querySelector(`.card.${contentKey}`)
         const currentHrsEl = contentEl.children[0].children[2]
@@ -33,7 +58,6 @@ async function loadData() {
         currentHrsEl.textContent = `${timeframes[dashboardTimeframe].current}hrs`
         previousTypeEl.textContent = ['weekly', 'monthly'].includes(dashboardTimeframe) ? dashboardTimeframe.slice(0, -2) : 'day'
         previousHrsEl.textContent = `${timeframes[dashboardTimeframe].previous}hrs`
-        console.log('hhh', previousTypeEl)
       }
     })
     .catch((error) => console.log(error))
